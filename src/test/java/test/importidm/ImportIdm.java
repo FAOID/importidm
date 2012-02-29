56df02152776a60642ed9b235f017ceb44fa3698
Eko Suprapto Wibowo <swdev.bali@gmail.com>
2012-02-29 09:13:33 +0700

    first project

---
+++ src/test/java/test/importidm/ImportIdm.java
@@ -0,0 +1,55 @@
+package test.importidm;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openforis.collect.persistence.RecordDAO;
+import org.openforis.collect.persistence.SurveyDAO;
+import org.openforis.collect.persistence.SurveyImportException;
+import org.openforis.collect.persistence.xml.CollectIdmlBindingContext;
+import org.openforis.idm.metamodel.Survey;
+import org.openforis.idm.metamodel.xml.InvalidIdmlException;
+import org.openforis.idm.metamodel.xml.SurveyUnmarshaller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:test-context.xml" })
+@TransactionConfiguration(defaultRollback = false)
+@Transactional
+public class ImportIdm {
+
+	@Autowired
+	protected SurveyDAO surveyDao;
+
+	@Autowired
+	protected RecordDAO recordDao;
+
+	@Test
+	public void testImportSurvey() throws Exception {
+		// IMPORT MODEL
+		importModel();
+	}
+
+	private Survey importModel() throws IOException, SurveyImportException,
+			InvalidIdmlException {
+		File idmFile = new File(
+				"E:/work/workspace/importidm/src/main/resources/idnfi.idm.xml");
+		FileInputStream is = new FileInputStream(idmFile);
+		// URL idm = ClassLoader.getSystemResource("test.idm.xml");
+		// InputStream is = idm.openStream();
+		CollectIdmlBindingContext idmlBindingContext = new CollectIdmlBindingContext();
+		SurveyUnmarshaller surveyUnmarshaller = idmlBindingContext
+				.createSurveyUnmarshaller();
+		Survey survey = surveyUnmarshaller.unmarshal(is);
+		surveyDao.importModel(survey);
+		return survey;
+	}
+
+}
