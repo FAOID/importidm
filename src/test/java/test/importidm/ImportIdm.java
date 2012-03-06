da6afd99861536cbcaf45093c451fec92230d803
Eko Suprapto Wibowo <swdev.bali@gmail.com>
2012-03-06 20:20:16 +0700

    *move context.xml to resources, and updated Confit to /

--- src/test/java/test/importidm/ImportIdm.java
+++ src/test/java/test/importidm/ImportIdm.java
@@ -6,13 +6,17 @@ import java.io.IOException;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.openforis.collect.persistence.RecordDAO;
-import org.openforis.collect.persistence.SurveyDAO;
+
+import org.openforis.collect.model.CollectSurveyContext;
+import org.openforis.collect.persistence.RecordDao;
+import org.openforis.collect.persistence.SurveyDao;
 import org.openforis.collect.persistence.SurveyImportException;
 import org.openforis.collect.persistence.xml.CollectIdmlBindingContext;
 import org.openforis.idm.metamodel.Survey;
+import org.openforis.idm.metamodel.validation.Validator;
 import org.openforis.idm.metamodel.xml.InvalidIdmlException;
 import org.openforis.idm.metamodel.xml.SurveyUnmarshaller;
+import org.openforis.idm.model.expression.ExpressionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -20,16 +24,20 @@ import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
+@ContextConfiguration(locations={"/ImportIdm-context.xml"})
 @TransactionConfiguration(defaultRollback = false)
 @Transactional
 public class ImportIdm {
 
 	@Autowired
-	protected SurveyDAO surveyDao;
+	protected SurveyDao surveyDao;
 
 	@Autowired
-	protected RecordDAO recordDao;
+	protected RecordDao recordDao;
+	@Autowired
+	protected ExpressionFactory expressionFactory;
+	@Autowired
+	protected Validator validator;
 
 	@Test
 	public void testImportSurvey() throws Exception {
@@ -41,10 +49,11 @@ public class ImportIdm {
 		File idmFile = new File(
 				"E:\\work\\importidm\\src\\main\\resources\\simple.idm.xml");
 		FileInputStream is = new FileInputStream(idmFile);
-		CollectIdmlBindingContext idmlBindingContext = new CollectIdmlBindingContext();
-		SurveyUnmarshaller surveyUnmarshaller = idmlBindingContext.createSurveyUnmarshaller();
+		CollectIdmlBindingContext idmlBindingContext = new CollectIdmlBindingContext(new CollectSurveyContext(expressionFactory, validator));
+		SurveyUnmarshaller surveyUnmarshaller = idmlBindingContext
+				.createSurveyUnmarshaller();
 		Survey survey = surveyUnmarshaller.unmarshal(is);
-		surveyDao.clearModel();
+		//surveyDao.clearModel();
 		survey.setName("idnfi");
 		surveyDao.importModel(survey);
 		return survey;
