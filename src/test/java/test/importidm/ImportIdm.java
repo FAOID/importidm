2e2afa66cc0c4c67938d90b63bcfb49c3f864f12
Eko Suprapto Wibowo <swdev.bali@gmail.com>
2012-04-03 14:23:25 +0700

    - ID Mofor IDM update pattern is MOFOR_date_update.idnfi.idm.xml

--- src/test/java/test/importidm/ImportIdm.java
+++ src/test/java/test/importidm/ImportIdm.java
@@ -39,11 +39,14 @@ public class ImportIdm {
 	@Autowired
 	protected Validator validator;
 
-	@Test
+	/* @Test */
 	public void testImportSurvey() throws Exception {
 		importModel();
 	}
 
+	/*
+	 * This will completely erase the database
+	 */
 	private Survey importModel() throws IOException, SurveyImportException,
 			InvalidIdmlException {
 		URL idm = ClassLoader.getSystemResource("idnfi.idm.xml");
@@ -54,6 +57,26 @@ public class ImportIdm {
 		survey.setName("idnfi");
 		surveyDao.clearModel();
 		surveyDao.importModel(survey);
+		return survey;
+	}
+	
+	@Test
+	public void testUpdateSurvey() throws IOException, InvalidIdmlException, SurveyImportException {
+		updateModel();
+	}
+
+	/*
+	 * Eko 14:19 03/04/2012 Allow adding of new fields only. Entity renaming not supported. Deletion not yet researched
+	 */
+	private Survey updateModel() throws IOException, InvalidIdmlException, SurveyImportException {
+		
+		URL idm = ClassLoader.getSystemResource("MOFOR_2012_04_03_update.idnfi.idm.xml");
+		InputStream is = idm.openStream();
+		CollectIdmlBindingContext idmlBindingContext = surveyDao.getBindingContext();
+		SurveyUnmarshaller surveyUnmarshaller = idmlBindingContext.createSurveyUnmarshaller();
+		CollectSurvey survey = (CollectSurvey) surveyUnmarshaller.unmarshal(is);
+		survey.setName("idnfi");
+		surveyDao.updateModel(survey);
 		return survey;
 	}
 
