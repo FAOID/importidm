23ba59c5c65c09d54c38eee6e02c43935448fc5f
Eko Suprapto Wibowo <swdev.bali@gmail.com>
2012-04-12 09:33:33 +0700

    +rename label from part into largepart/small part. But haven't renamed the entity though, Still part. Must rename it manually in the ofc_schema_definition

--- src/test/java/test/importidm/ImportIdm.java
+++ src/test/java/test/importidm/ImportIdm.java
@@ -14,36 +14,37 @@ import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"/ImportIdm-context.xml"})
+@ContextConfiguration(locations = { "/ImportIdm-context.xml" })
 @TransactionConfiguration(defaultRollback = false)
-
 /**
  * @author Wibowo, Eko
  */
 @Transactional
 public class ImportIdm {
 
-	
 	@Autowired
 	protected IdmUpdater idmUpdater;
-	
 
-	/* disabled.
-	 * UpdateModel has all the functionality of importModel, with added feature of updating database structure into the newest IDM
-	 * I'll keep the method here for historical purpose only  
-	public void testImportIdnfi() throws Exception {
-		idmUpdater.importIdnfi("idnfi", ClassLoader.getSystemResource("idnfi.idm.xml"));
-	}*/
-
+	/*
+	 * disabled. UpdateModel has all the functionality of importModel, with
+	 * added feature of updating database structure into the newest IDM I'll
+	 * keep the method here for historical purpose only public void
+	 * testImportIdnfi() throws Exception { idmUpdater.importIdnfi("idnfi",
+	 * ClassLoader.getSystemResource("idnfi.idm.xml")); }
+	 */
 
 	@Test
-	public void updateIdnfiIdm() throws IOException, InvalidIdmlException, SurveyImportException {
-		idmUpdater.updateModel("idnfi",ClassLoader.getSystemResource("MOFOR_WORKING.idnfi.idm.xml"));
+	public void updateIdnfiIdm() throws IOException, InvalidIdmlException,
+			SurveyImportException {
+		idmUpdater.updateModel("idnfi",
+				ClassLoader.getSystemResource("MOFOR_WORKING.idnfi.idm.xml"));
 	}
-	
-	@Test
-	public void updateGreenbookIdm() throws IOException, InvalidIdmlException, SurveyImportException {
-		idmUpdater.updateModel("greenbook",ClassLoader.getSystemResource("MOFOR_WORKING.greenbook.idm.xml"));
+
+	//@Test
+	public void updateGreenbookIdm() throws IOException, InvalidIdmlException,
+			SurveyImportException {
+		idmUpdater.updateModel("greenbook", ClassLoader
+				.getSystemResource("MOFOR_WORKING.greenbook.idm.xml"));
 	}
-	
+
 }
