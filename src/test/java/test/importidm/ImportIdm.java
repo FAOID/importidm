abb6b1a7fb1b5dd17b757fb45ee82b055d2eeb7c
Eko Suprapto Wibowo <swdev.bali@gmail.com>
2012-06-07 14:47:17 +0700

    Focus on starting to upgrade the use of Taxon element using qualifierExpression
    Signed-off-by: Eko Suprapto Wibowo <swdev.bali@gmail.com>

--- src/test/java/test/importidm/ImportIdm.java
+++ src/test/java/test/importidm/ImportIdm.java
@@ -51,7 +51,7 @@ public class ImportIdm {
 	@Test
 	public void updateIdnfiIdm() throws IOException, InvalidIdmlException, SurveyImportException {
 	
-		InputStream is = ClassLoader.getSystemResource("MOFOR_WORKING_REMOVING_PROVINCE.idnfi.idm.xml").openStream();
+		InputStream is = ClassLoader.getSystemResource("MOFOR_WORKING.idnfi.idm.xml").openStream();
 		CollectIdmlBindingContext idmlBindingContext = surveyDao.getBindingContext();
 		SurveyUnmarshaller surveyUnmarshaller = idmlBindingContext.createSurveyUnmarshaller();
 		CollectSurvey survey = (CollectSurvey) surveyUnmarshaller.unmarshal(is);
@@ -72,7 +72,7 @@ public class ImportIdm {
 		surveyDao.updateModel(survey);
 	}
 	
-	@Test
+	//@Test
 	public void storeLogo() throws IOException
 	{
 		byte[] image = null;
